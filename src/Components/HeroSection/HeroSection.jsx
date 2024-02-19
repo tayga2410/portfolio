ff379d6e2c3048e1708b0afc4032ec85f38c9c9a
tayga2410 <moldazhanov.eldar@gmail.com>
2024-02-19 22:51:48 +0600

    text translation for hero section

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import './herosection.css';
-import hero from "../../img/hero.jpg";
+import React, { useState, useEffect } from "react";
+import "./herosection.css";
+import hero from "../../img/hero.png";
+import { useTranslation } from 'react-i18next';
 
 const HeroSection = () => {
+    const { t } = useTranslation();
   const [displayText, setDisplayText] = useState("");
   const introText = "Hi! I'm Eldar and I'm ";
-  const phrasesToType = ["Junior Frontend Developer", "Quick Learner", "not afraid of something new"];
+  const phrasesToType = [
+    "Junior Frontend Developer",
+    "Quick Learner",
+    "not afraid of something new",
+  ];
   const [phraseIndex, setPhraseIndex] = useState(0);
 
   useEffect(() => {
@@ -14,13 +20,15 @@ const HeroSection = () => {
 
     const typingInterval = setInterval(() => {
       if (index < currentPhrase.length) {
-        setDisplayText(prevText => introText + currentPhrase.substring(0, index + 1));
+        setDisplayText(
+          (prevText) => introText + currentPhrase.substring(0, index + 1)
+        );
         index++;
       } else {
         clearInterval(typingInterval);
         setTimeout(() => {
           setDisplayText(introText);
-          setPhraseIndex(prevIndex => (prevIndex + 1) % phrasesToType.length);
+          setPhraseIndex((prevIndex) => (prevIndex + 1) % phrasesToType.length);
         }, 2000);
       }
     }, 125);
@@ -30,14 +38,18 @@ const HeroSection = () => {
 
   return (
     <section className="hero__container">
-        <div className='hero__text-container'>
-      <h2 className="hero__title">Welcome to my Portfolio</h2>
-      <h3 className="hero__sub-title">{displayText}</h3>
-      <p className="hero__description">
-        I think the background of a person is very important, but a lot of text is also considered as "not good". I'm a former businessman, but now I decided to change my life dramatically and I'm passionate about possessing new information about frontend development and IT sphere in general
-      </p>
+      <div className="hero__text-container">
+        <h2 className="hero__title">{t("welcome")}</h2>
+        <h3 className="hero__sub-title">{displayText}</h3>
+        <p className="hero__description">{t("hero-description")}</p>
       </div>
-      <img className="hero__image" src={hero} alt={"coffee with keyboard"} width="600" height="461"/>
+      <img
+        className="hero__image"
+        src={hero}
+        alt={"Kazakhstan flag"}
+        width="600"
+        height="461"
+      />
     </section>
   );
 };
